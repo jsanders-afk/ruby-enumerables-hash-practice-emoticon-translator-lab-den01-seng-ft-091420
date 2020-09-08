c511fe1617c190893352589cc08a7247aa6995d0
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-08 05:30:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,9 +24,9 @@ end
 
 
 def get_english_meaning(emoticons, emote)
-  arg = Proc.new {"Lo matches found"}
+  arg = Proc.new {"No matches found"}
   load_library(emoticons)
-  found = load_library(emoticons).find(arg) do |emotname, eoj|
+  found = load_library(emoticons).find(arg.to_s) do |emotname, eoj|
     load_library(emoticons)[emotname][:japanese] == emote
           end
   found[0]        

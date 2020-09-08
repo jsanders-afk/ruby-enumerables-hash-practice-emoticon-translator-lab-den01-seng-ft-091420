967c7e196afad91487f40c08d51da4af975db886
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-08 04:58:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,8 @@ end
 
 def get_english_meaning(emoticons, emote)
   load_library(emoticons).find do |emotname, eoj|
-    load_library(emoticons)[emotname][:japanese] = emote
+    found = load_library(emoticons)[emotname][:japanese] == emote
+    
   end
 end
 binding.pry

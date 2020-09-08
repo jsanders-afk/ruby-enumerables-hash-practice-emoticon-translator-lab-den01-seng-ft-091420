4713d00ca40407ed918bc475709bb2f960cee218
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-08 04:48:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,48 @@
-# require modules here
+require "yaml"
+require 'pry'
+emoticons = YAML.load_file("./lib/emoticons.yml")
 
-def load_library
-  # code goes here
+def load_library(emoticons)
+  emoticons = YAML.load_file("./lib/emoticons.yml")
+  eng = []
+  jap = []
+  emoticons.each do |emot_name, the_emotes|
+    the_emotes.each_with_index do |item,index| 
+      if (index %2 ==0) then 
+      eng.push(item)
+      else
+      jap.push(item)
+      end
+    end
+  end
+  ultimate = emoticons.each_with_object({}) do |(emot_name, the_emotes), big_hash|
+    big_hash[emot_name] = {}
+    big_hash[emot_name] = {:english => eng[big_hash.length - 1], :japanese => jap[big_hash.length - 1]}
+  end
+  ultimate
 end
+
+
+def get_english_meaning(emoticons, emote)
+  p emote
+  load_library(emoticons).find do |emotname, eoj|
+    load_library(emoticons)[emotname][:japanese] == emote
+  end
+end
+
+
+ 
+
+#eoj.find do |lan, sym|
+#      load_library(emoticons)[eoj][sym] == emote
+#    end
+
+
+
+
 
 def get_japanese_emoticon
-  # code goes here
 end
 
-def get_english_meaning
-  # code goes here
-end
+
+
